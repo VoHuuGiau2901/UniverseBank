06f6447fca818a80d99d52c6abe1abaa009b3a32
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-11-25 11:57:58 +0700

    Initial commit

---
+++ src/main/java/com/ec/final_project/FiNAlPrOjEctApplication.java
@@ -0,0 +1,13 @@
+package com.ec.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiNAlPrOjEctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiNAlPrOjEctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ec/final_project/FiNAlPrOjEctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiNAlPrOjEctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
