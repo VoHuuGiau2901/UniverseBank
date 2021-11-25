661ccbcda44ca2a5c74f715077d182151708da9d
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-11-25 22:13:12 +0700

    login VAliĐaTe account used or not addede

--- src/main/java/com/ec/final_project/Controllers/thongtintkController.java
+++ src/main/java/com/ec/final_project/Controllers/thongtintkController.java
@@ -26,7 +26,7 @@ public class thongtintkController {
     @ResponseBody
     public thongtintk logIn(@RequestBody thongtintk tk) {
         if (tkService.timthongtin(tk) != null) {
-            return tkService.timthongtin(tk)s;
+            return tkService.timthongtin(tk);
         } else return null;
     }
 }
