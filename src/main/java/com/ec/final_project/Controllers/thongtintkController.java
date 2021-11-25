d28f14d1c8a4ebf33fcc3a551b10c87f194f8204
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-11-25 16:09:10 +0700

    Register FUnction addede

--- src/main/java/com/ec/final_project/Controllers/thongtintkController.java
+++ src/main/java/com/ec/final_project/Controllers/thongtintkController.java
@@ -3,13 +3,11 @@ package com.ec.final_project.Controllers;
 import com.ec.final_project.Beans.thongtintk;
 import com.ec.final_project.Services.thongtintkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Welcome")
+@CrossOrigin
 public class thongtintkController {
 
     @Autowired
