3010dacebfb06f83407cfbc646db88fa5c1581dc
phianh5@gmail.com <phianh5@gmail.com>
2021-11-28 12:12:03 +0700

    add Paypal utils, execution

---
+++ src/main/java/com/ec/final_project/Paypal/Config/PaypalPaymentIntent.java
@@ -0,0 +1,5 @@
+package com.ec.final_project.Paypal.Config;
+
+public enum PaypalPaymentIntent {
+    sale, authorize, order
+}
---
+++ src/main/java/com/ec/final_project/Paypal/Config/PaypalPaymentMethod.java
@@ -0,0 +1,5 @@
+package com.ec.final_project.Paypal.Config;
+
+public enum PaypalPaymentMethod {
+    credit_card, paypal
+}
