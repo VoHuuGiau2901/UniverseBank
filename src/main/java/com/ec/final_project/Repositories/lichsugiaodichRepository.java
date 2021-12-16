6ba51307e25c162b94adb6ac3ab46cbf48648c0d
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-16 12:26:35 +0700

    add calculate % stonk compare with last month

--- src/main/java/com/ec/final_project/Repositories/lichsugiaodichRepository.java
+++ src/main/java/com/ec/final_project/Repositories/lichsugiaodichRepository.java
@@ -12,6 +12,11 @@ public interface lichsugiaodichRepository extends JpaRepository<lichsugiaodich,I
 
     @Query(value = "SELECT (SELECT count(DISTINCT tk.Acc_id) from thongtintk tk)a,\n" +
             "       (SELECT sum(tkTK.SoTien) as total_payment_money FROM taikhoan_tietkiem tkTK)b,\n" +
-            "       (SELECT sum(tkTT.SoTien) FROM taikhoan_thanhtoan tkTT)c;",nativeQuery = true)
+            "       (SELECT sum(tkTT.SoTien) FROM taikhoan_thanhtoan tkTT)c,\n" +
+            "       (SELECT sum(lsgd.SoTienGiaoDich)\n" +
+            "        FROM lichsugiaodich lsgd\n" +
+            "        WHERE YEAR(lsgd.Ngay) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)\n" +
+            "          AND MONTH(lsgd.Ngay) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)\n" +
+            "          AND lsgd.HinhThuc=1)d",nativeQuery = true)
     Object get_total_user_and_money();
 }
