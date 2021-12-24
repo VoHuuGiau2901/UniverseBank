fbc4ea3dd2b53cb790aa413bd4336f570f1dbd9c
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-25 00:01:35 +0700

    update

--- src/main/java/com/ec/final_project/check.java
+++ src/main/java/com/ec/final_project/check.java
@@ -14,8 +14,8 @@ public class check extends Thread {
         while (true) {
             try {
                 checkModel.check_and_update();
-//                Thread.sleep(86400000);  // official
-                Thread.sleep(60000); // test
+                Thread.sleep(86400000);  // official
+//                Thread.sleep(60000); // test
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
