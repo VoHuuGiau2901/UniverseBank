b2bb8cdaf1f36f6cd1fb04178c0467a319660209
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-26 00:09:24 +0700

    fix bug

--- src/main/java/com/ec/final_project/check.java
+++ src/main/java/com/ec/final_project/check.java
@@ -14,6 +14,7 @@ public class check extends Thread {
         while (true) {
             try {
                 checkModel.check_and_update();
+
                 Thread.sleep(86400000);  // official
 //                Thread.sleep(60000); // test
             } catch (InterruptedException e) {
